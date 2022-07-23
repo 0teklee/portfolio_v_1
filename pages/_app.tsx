c078ba1941482808bf9ee37d553ae86d9e54435d
0teklee <leetekwoo@gmail.com>
2022-07-23 23:33:30 +0900

    fix: eslint fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,22 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ThemeProvider} from "styled-components";
-import {GlobalStyles} from "../styles/global-styles";
-import {theme } from '../styles/theme'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styles/global-styles";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <title>app</title>
-        </Head>
-        <GlobalStyles/>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </>)
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>app</title>
+      </Head>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
