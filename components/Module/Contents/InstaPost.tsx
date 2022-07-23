1cf0da1301b37a2f04ad2fa6d22993faf4fc3a2b
0teklee <leetekwoo@gmail.com>
2022-07-24 03:47:10 +0900

    fix: insta post layout

--- components/Module/Contents/InstaPost.tsx
+++ components/Module/Contents/InstaPost.tsx
@@ -11,7 +11,7 @@ const InstaPost = () => {
       <Title marginBottom="0.5rem">Instagram</Title>
       <__A href={igLink}>@lesyeoux</__A>
       <__SlideWrapper>
-        <Slide>
+        <Slide interval={8000}>
           {instaData.map((post) => (
             <Item key={post.id} img={post.url} url={igLink} />
           ))}
@@ -40,6 +40,6 @@ const __A = styled.a`
 
 const __SlideWrapper = styled.div`
   width: 100%;
-  margin: 1.5rem;
+  margin: 1.5rem 0;
   cursor: pointer;
 `;
--- public/asset/InstaPost.ts
+++ public/asset/InstaPost.ts
@@ -1,11 +1,11 @@
 const instaData = [
   {
     id: 1,
-    url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.29350-15/289934134_441620794158543_5621555756035639737_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=SFqVuV1gEY4AX8oAXol&_nc_oc=AQkU4I963rF5FSu1foWCHchoLORCKR6OQoNEpVIoDqhsva172ucO_X10Rir5OcU27bQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-qriwu9CZNjIHcyrMIAxfhaCdvoPl1dznsq6gCy5RwrA&oe=62E08C5A",
+    url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-15/272284490_624309448805798_6040205860195248592_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=hLsDK9rjl9sAX_OcTEg&tn=Wk0NDv3mX9OqGKcP&edm=AGenrX8BAAAA&ccb=7-5&oh=00_AT9fNIDLOc4-5a8JMxSio4PUvxaTHruPya4kJ7AhhjL4iw&oe=62E30C65&_nc_sid=5eceaa",
   },
   {
     id: 2,
-    url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.29350-15/285047838_529447885314242_3960395274006851567_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=RGlKCLGF6qIAX9D5zvp&_nc_ht=scontent-gmp1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8eZGXC3ldv62SjO3n-RnMjHPmYVLIWiqIWFcxLD-CTFg&oe=62E0B84B",
+    url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.29350-15/289934134_441620794158543_5621555756035639737_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=SFqVuV1gEY4AX8oAXol&_nc_oc=AQkU4I963rF5FSu1foWCHchoLORCKR6OQoNEpVIoDqhsva172ucO_X10Rir5OcU27bQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-qriwu9CZNjIHcyrMIAxfhaCdvoPl1dznsq6gCy5RwrA&oe=62E08C5A",
   },
   {
     id: 3,
@@ -17,6 +17,10 @@ const instaData = [
   },
   {
     id: 5,
+    url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.29350-15/285047838_529447885314242_3960395274006851567_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=RGlKCLGF6qIAX9D5zvp&_nc_ht=scontent-gmp1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8eZGXC3ldv62SjO3n-RnMjHPmYVLIWiqIWFcxLD-CTFg&oe=62E0B84B",
+  },
+  {
+    id: 6,
     url: "https://scontent-gmp1-1.cdninstagram.com/v/t51.29350-15/278761350_314382670805515_4762710897828046490_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=wW7vkxNPztkAX_XNBws&_nc_ht=scontent-gmp1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-7p9Ut5wIX5dL-p7aQw7NrbpT3RIKItZ-YRLB0ExscHA&oe=62E1969F",
   },
   {
