041c1d2336605ac80033a770aeeac7492102f47a
0teklee <leetekwoo@gmail.com>
2022-07-24 03:29:53 +0900

    fix: lint error metatg

--- components/Templates/MobileMain/MobileMainTemplate.tsx
+++ components/Templates/MobileMain/MobileMainTemplate.tsx
@@ -2,7 +2,6 @@ import Footer from "components/Module/Section/Footer";
 import Layout from "../../Layout/Layout";
 import MobileMainFullTemplate from "./MobileMainFullTemplate";
 import HeaderMobile from "../../Module/Section/HeaderMobile";
-import MetaTag from "components/MetaTag";
 
 const MobileMainTemplate = () => {
   return (
